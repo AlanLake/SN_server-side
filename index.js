6179099e7d577e82ff2be03b9acd14c162c8a2e2
AlanLake <alan.lake@mail.ru>
2021-01-23 20:51:52 +0300

    deploying server

---
+++ index.js
@@ -0,0 +1,30 @@
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
+const pubsub = new PubSub();
+
+const server = new ApolloServer({
+  connectToDevTools: true,
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub }),
+});
+
+const PORT = process.env.port || 5000;
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server
+      .listen({ PORT })
+      .then((res) => {
+        console.log(`Server running at ${res.url}`);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
